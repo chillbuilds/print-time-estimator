41645f1a529783f6a1229ae77e4b8c07ce922e97
heliumface770 <heliumface770@gmail.com>
2019-12-02 23:58:22 -0700

    page populated

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,38 @@
+//Dropdowns to build list for estimate
+
+wood
+    credenza
+    round table
+    coffee table
+    round side
+    square side
+    square cutting boards
+    round cutting boards
+    rectangle cutting boards
+    3x3
+    4x4
+    5x5
+
+    
+vinyl
+    clocks
+    trinket tray
+    rec tray lg
+    rec tray xl
+    8 x 9.5
+    11 x 13
+    14 x 16.5
+    19 x 22.5
+    12 x 12
+    16 x 16 (lg sq tray)
+    20 x 20
+    30 x 30
+
+misc
+    benches
+    stools
+    welcome mats sm
+    welcome mats lg
+    acrylic box
+    acrylic tray sm
+    acrylic tray lg
