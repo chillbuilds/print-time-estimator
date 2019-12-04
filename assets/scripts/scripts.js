3de36e46b83ad367354081b629fe751b03b04ddd
heliumface770 <heliumface770@gmail.com>
2019-12-03 23:39:11 -0700

    updated multipliers, and icon added for default multipliers

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,6 +1,6 @@
 var products = [
-  { name: "Credenza", material: "wood", multiplier: 300 },
-  { name: "Round Table", material: "wood", multiplier: 100 },
+  { name: "Credenza", material: "wood", multiplier: 1 },
+  { name: "Round Table", material: "wood", multiplier: 1 },
   { name: "Coffee Table", material: "wood", multiplier: 1 },
   { name: "Round Side", material: "wood", multiplier: 1 },
   { name: "Square Side", material: "wood", multiplier: 1 },
@@ -12,16 +12,16 @@ var products = [
   { name: "5 X 5 Mural", material: "wood", multiplier: 1 },
   { name: "Clocks", material: "vinyl", multiplier: 1 },
   { name: "Trinket Tray", material: "vinyl", multiplier: 1 },
-  { name: "Rec Tray Lg", material: "vinyl", multiplier: 1 },
-  { name: "Rec Tray XL", material: "vinyl", multiplier: 1 },
-  { name: "8 X 9.5", material: "vinyl", multiplier: 1 },
+  { name: "Rec Tray Lg", material: "vinyl", multiplier: 21 },
+  { name: "Rec Tray XL", material: "vinyl", multiplier: 28 },
+  { name: "8 X 9.5", material: "vinyl", multiplier: 16 },
   { name: "11 X 13", material: "vinyl", multiplier: 1 },
-  { name: "14 X 16.5", material: "vinyl", multiplier: 1 },
-  { name: "19 X 22.5", material: "vinyl", multiplier: 1 },
-  { name: "12 X 12", material: "vinyl", multiplier: 1 },
+  { name: "14 X 16.5", material: "vinyl", multiplier: 22 },
+  { name: "19 X 22.5", material: "vinyl", multiplier: 26 },
+  { name: "12 X 12", material: "vinyl", multiplier: 18 },
   { name: "Lg Sq Tray", material: "vinyl", multiplier: 1 },
-  { name: "20 X 20", material: "vinyl", multiplier: 1 },
-  { name: "30 X 30", material: "vinyl", multiplier: 1 },
+  { name: "20 X 20", material: "vinyl", multiplier: 25 },
+  { name: "30 X 30", material: "vinyl", multiplier: 42 },
   { name: "Benches", material: "misc", multiplier: 1 },
   { name: "Stools", material: "misc", multiplier: 1 },
   { name: "Welcome Mats Sm", material: "misc", multiplier: 1 },
@@ -30,21 +30,20 @@ var products = [
   { name: "Acrylic Tray Sm", material: "misc", multiplier: 1 },
   { name: "Acrylic Tray Lg", material: "misc", multiplier: 1 }
 ];
-
 var totalSeconds = 0;
 
 //loop through and populate "#wood" div
 for (var i = 0; i < products.length; i++) {
     var product = $("<div>");
     product.addClass("inputs");
+    if(products[i].multiplier !== 1){
+    product.html('<input type="number" id="product-' + i + '">' + products[i].name);}
+    else{product.html('<input type="number" id="product-' + i + '">' + products[i].name + '<img src="./assets/images/icons/yield.png" class="icon">');}
   if (products[i].material === "wood") {
-    product.html('<input type="number" id="wood-' + i + '">' + products[i].name);
     $("#wood").append(product);
   } else if (products[i].material === "vinyl") {
-    product.html('<input type="number" id="vinyl-' + i + '">' + products[i].name);
     $("#vinyl").append(product);
   }else{
-  product.html('<input type="number" id="misc-' + i + '">' + products[i].name);
   $("#misc").append(product);}
 }
 
@@ -53,16 +52,14 @@ for (var i = 0; i < products.length; i++) {
 $("#calculate").on("click", function() {
   totalSeconds = 0;
   for (var i = 0; i < products.length; i++) {
-    var divId = products[i].material + "-" + i;
+    var divId = "product" + "-" + i;
     var multiplier = products[i].multiplier;
     var x = $("#" + divId).val();
     var input = parseInt(x);
     if(input > 0){totalSeconds = totalSeconds + (input*multiplier);}
   }
-  
+  alert(totalSeconds);
   const formatted = moment.utc(totalSeconds*1000).format('HH:mm:ss');
-  console.log(formatted);
-  var j = formatted.split(":")
-  console.log(j);
-  $("#estimate").text(j[0]+"hrs "+j[1]+"mins");
+  var timeArr = formatted.split(":")
+  $("#estimate").text(timeArr[0]+"hrs "+timeArr[1]+"mins");
 });
